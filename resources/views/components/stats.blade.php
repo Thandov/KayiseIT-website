d209643477a64b9af757e5420fa4a947ead25106
KrayATM <kgothatsotloubatla96@gmail.com>
2023-04-08 13:13:23 +0200

    Kagiso Minor Changes
    Created dark green style
    Added Dark green styling id

--- resources/views/components/stats.blade.php
+++ resources/views/components/stats.blade.php
@@ -6,27 +6,27 @@
                 <h2 data-aos="fade-down" data-aos-delay="500" class="bigtxt font-bold text-5xl">We’re on a mission to bridge the digital divide</h2>
             </div>
         </div>
-        <!--Overview-->
+         <!--Overview-->
         <div class="overviews grid md:grid-cols-4 sm:grid-cols-2">
             <!--No.1 Years-->
             <div class="overview" style="color: #183ea4" data-aos="fade-right" data-aos-delay="500">
                 <span>{{$years}}</span>
-                <p>YEARS</p>
+                <p id="dark-g-text">YEARS</p>
             </div>
             <!--No.2 Developers-->
             <div class="overview" style="color: #183ea4" data-aos="fade-down" data-aos-delay="700">
                 <span>{{$developers}}</span>
-                <p>DEVELOPERS</p>
+                <p id="dark-g-text">DEVELOPERS</p>
             </div>
             <!--No.3 Customer-->
             <div class="overview" style="color: #183ea4" data-aos="fade-up" data-aos-delay="1100">
                 <span>{{$customers}}</span>
-                <p>CUSTOMERS</p>
+                <p id="dark-g-text">CUSTOMERS</p>
             </div>
             <!--No.3 Projects-->
             <div class="overview" style="color: #183ea4" data-aos="fade-left" data-aos-delay="1500">
                 <span>{{$projects}}</span>
-                <p>PROJECTS</p>
+                <p id="dark-g-text">PROJECTS</p>
             </div>
         </div>
     </div>
