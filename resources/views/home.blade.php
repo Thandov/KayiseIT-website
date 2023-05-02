915720cb23f59167d07c666f609e48ea4bdf5a24
KrayATM <kgothatsotloubatla96@gmail.com>
2023-05-02 14:35:48 +0200

    Kagiso - About Us section Redesign
    Created a new blade.php file for about us section
    -Made changes to the layout
    -Added New images
    -Did a basic responsive styling for mobile view.
    -Added margins for clean look

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,20 +10,7 @@
     <!--freatured services-->
     @include('services._services')
     <!--About Us-->
-    <section class="bg-white">
-        <div class="container p-5" id="about-us">
-            <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-6 ">
-                    <img src="../images/KayiseIT-Team.jpg" width="900" class="rounded shadow">
-                </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 d-flex align-items-center">
-                    <div class="">
-                        <x-body smheading="About Us" bgheading="We Specialize In Custom Tailored I.T Solutions." paragraph="Welcome to KAYISE IT, a leading IT company specializing in software and web development, as well as providing 4IR skills training. With a decade of experience in the industry, we are passionate about building ICT capacity in communities and young people." btnlink="about" btntext="Discover More" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('_about')
     @include ('components.our-team')
     <!--Services Call-To-Action-->
     <section id="our-services">
