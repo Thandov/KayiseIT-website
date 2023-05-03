7e2c598603a0464f46ae8c7803309c20c0fed1b3
Maluka Thapelo <excellentt7@gmail.com>
2023-05-03 15:25:44 +0200

    Update _projects.blade.php
    added pictures for our projects section

--- resources/views/_projects.blade.php
+++ resources/views/_projects.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-2 flex justify-center px-40">
             <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/test.jpg')"
+                <div style="background-image: url('../images/inter.jpg')"
                     class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
                 <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
                     <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
@@ -16,7 +16,7 @@
                 </div>
             </div>
             <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/test.jpg')"
+                <div style="background-image: url('../images/network.png')"
                     class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
                 <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
                     <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
@@ -26,7 +26,7 @@
                 </div>
             </div>
             <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/test.jpg')"
+                <div style="background-image: url('../images/skills.jpg')"
                     class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
                 <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
                     <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
@@ -36,7 +36,7 @@
                 </div>
             </div>
             <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/test.jpg')"
+                <div style="background-image: url('../images/tech.png')"
                     class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
                 <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
                     <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
@@ -46,7 +46,7 @@
                 </div>
             </div>
             <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/test.jpg')"
+                <div style="background-image: url('../images/out.jpg')"
                     class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
                 <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
                     <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
@@ -56,7 +56,7 @@
                 </div>
             </div>
             <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/test.jpg')"
+                <div style="background-image: url('../images/contact.jpg')"
                     class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
                 <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
                     <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
