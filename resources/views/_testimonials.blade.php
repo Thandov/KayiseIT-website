4c7f11cd62f7f65c360c64a995acc9fe5852ec40
KrayATM <kgothatsotloubatla96@gmail.com>
2023-05-03 13:25:24 +0200

    Kagiso - Testimonials card
    - Updated the cards style
    - Updated the background

--- resources/views/_testimonials.blade.php
+++ resources/views/_testimonials.blade.php
@@ -1,22 +1,21 @@
 <!-- Section: Testimonials -->
-<section class="m-5">
-    <x-titlestyle smheading="What People Say" bgheading="Client Testimonials" alignment="text-center" smheadingcolor=""
-        bgheadingcolor=""></x-titlestyle>
-    <p class="mb-10 text-center">Below are a few statements from our clients sharing their experiences and feedback</p>
+<section class="testimonial py-3" id="testimonial_background">
+    <x-titlestyle smheading="What People Say" bgheading="Client Testimonials" alignment="text-center" smheadingcolor="" bgheadingcolor=""></x-titlestyle>
+    <p class="mb-10 text-center text-white">Below are a few statements from our clients sharing their experiences and feedback</p>
     <!--Carousel-->
     <div id="default-carousel" class="relative w-full" data-carousel="slide">
         <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div class="relative h-96 overflow-hidden md:h-60  mx-5">
             <!-- Item 1 -->
             @foreach($testimonials as $testimonial)
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <!--Content-->
-                <div class="container flex justify-center px-8">
-                        <div class="columns-2 bg-white drop-shadow-lg w-1/2 p-4 flex justify-center">
-                            <div class="container flex items-center ">
-                                <img class="mx-auto rounded-full shadow-lg" src="{{ asset('images/testimonials/'.$testimonial->icon) }}" alt="avatar" style="width:120px; height:120px" />
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <!--Content-->
+                    <div class="max-w-md mx-auto bg-white shadow-md md:max-w-2xl rounded-md">
+                        <div class="md:flex">
+                            <div class="md:shrink-0 flex justify-center items-center p-3">
+                                <img class="h-28 w-28 object-cover md:h-28 md:w-28 rounded-full shadow-md" src="{{ asset('images/testimonials/'.$testimonial->icon) }}" alt="Kayise IT Testimonial Pic" />
                             </div>
-                            <div class="container text-left">
+                            <div class="p-3">
                                     @php
                                         $avg_rating = DB::table('testimonials')->where('id',$testimonial->id)->avg('ratings');
                                     @endphp
@@ -26,13 +25,13 @@
                                             <i class="far fa-star bigtxt"></i>
                                         @endif
                                     @endfor
-                                <p class="mt-1">{{ $testimonial['testimony'] }}</p>
-                                <p class="mt-3 smalltxt"><strong>{{ $testimonial['name'] }}</strong></p>
+                                <p class="mt-2 text-sm">{{ $testimonial['testimony'] }}</p>
+                                <p class="mt-2 smalltxt"><strong>{{ $testimonial['name'] }}</strong></p>
                                 <p>Position</p>
                             </div>
                         </div>
+                    </div>
                 </div>
-        </div>
             @endforeach
         <!-- Slider indicators -->
         <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
@@ -44,25 +43,19 @@
                 data-carousel-slide-to="2"></button>
         </div>
         <!-- Slider controls -->
-        <button type="button"
-            class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-            data-carousel-prev>
+        <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
             <span
                 class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none"
-                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
                 </svg>
                 <span class="sr-only">Previous</span>
             </span>
         </button>
-        <button type="button"
-            class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-            data-carousel-next>
+        <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
             <span
                 class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none"
-                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                 </svg>
                 <span class="sr-only">Next</span>
