7db2a6402711aea078acb95fbce4f69e4a61e949
KenosiCharlie <73502020+KenosiCharlie@users.noreply.github.com>
2023-05-04 10:19:00 +0200

    Update _projects.blade.php
    updated project layout
    fixed the spacing and aligned the cards to the center

--- resources/views/_projects.blade.php
+++ resources/views/_projects.blade.php
@@ -1,71 +1,74 @@
 <section class="bg-white">
-    <div class="container mx-auto p-5 bg-white">
-        <div class="text-center items-center">
-            <p class="smalltxt font-bold"><strong>Our Projects</strong></p>
-            <h2 class="bigtxt font-bold text-5xl mb-3">Tailor-made projects</h2>
-        </div>
-        <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-2 flex justify-center px-40">
-            <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/inter.jpg')"
-                    class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
-                <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
-                    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
-                    <div class="flex items-center justify-between py-2 text-center ">
-                        <p class="text-sm ">Computer Productivity & Efficiency Training</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/network.png')"
-                    class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
-                <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
-                    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
-                    <div class="flex items-center justify-between py-2 text-center ">
-                        <p class="text-sm ">Computer Productivity & Efficiency Training</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/skills.jpg')"
-                    class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
-                <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
-                    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
-                    <div class="flex items-center justify-between py-2 text-center ">
-                        <p class="text-sm ">Computer Productivity & Efficiency Training</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/tech.png')"
-                    class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
-                <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
-                    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
-                    <div class="flex items-center justify-between py-2 text-center ">
-                        <p class="text-sm ">Computer Productivity & Efficiency Training</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/out.jpg')"
-                    class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
-                <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
-                    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
-                    <div class="flex items-center justify-between py-2 text-center ">
-                        <p class="text-sm ">Computer Productivity & Efficiency Training</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-                <div style="background-image: url('../images/contact.jpg')"
-                    class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
-                <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
-                    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
-                    <div class="flex items-center justify-between py-2 text-center ">
-                        <p class="text-sm ">Computer Productivity & Efficiency Training</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="container mx-auto p-5 bg-white">
+<div class="text-center items-center">
+        <p class="smalltxt font-bold"><strong>Our Projects</strong></p>
+        <h2 class="bigtxt font-bold text-5xl mb-3">Tailor-made projects</h2>
     </div>
+    <div class="flex justify-center">
+    <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-2 flex justify-center w-10/12">
+    <div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+  <div style="background-image: url('../images/inter.jpg')"
+       class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
+  <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
+    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
+    <div class="flex items-center justify-between py-2 text-center ">
+    <p class="text-sm ">Computer Productivity & Efficiency Training</p>
     </div>
+  </div>
+</div>
+<div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+  <div style="background-image: url('../images/network.png')"
+       class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
+  <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
+    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
+    <div class="flex items-center justify-between py-2 text-center ">
+    <p class="text-sm ">Computer Productivity & Efficiency Training</p>
+    </div>
+  </div>
+</div>
+<div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+  <div style="background-image: url('../images/skills.jpg')"
+       class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
+  <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
+    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
+    <div class="flex items-center justify-between py-2 text-center ">
+    <p class="text-sm ">Computer Productivity & Efficiency Training</p>
+    </div>
+  </div>
+</div>
+<div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+  <div style="background-image: url('../images/tech.png')"
+       class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
+  <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
+    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
+    <div class="flex items-center justify-between py-2 text-center ">
+    <p class="text-sm ">Computer Productivity & Efficiency Training</p>
+    </div>
+  </div>
+</div>
+<div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+  <div style="background-image: url('../images/out.jpg')"
+       class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
+  <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
+    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
+    <div class="flex items-center justify-between py-2 text-center ">
+    <p class="text-sm ">Computer Productivity & Efficiency Training</p>
+    </div>
+  </div>
+</div>
+<div class="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+  <div style="background-image: url('../images/contact.jpg')"
+       class="bg-gray-300 h-60 w-64 rounded-lg shadow-md bg-cover bg-center"></div>
+  <div class="w-56 md:w-50 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden">
+    <div class="py-2 text-center font-bold   tracking-wide text-gray-800 smalltxt">Development</div>
+    <div class="flex items-center justify-between py-2 text-center ">
+    <p class="text-sm ">Computer Productivity & Efficiency Training</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
 </section>
