9dba47949200083636bb1789d435698643aa58ea
KrayATM <kgothatsotloubatla96@gmail.com>
2023-05-05 12:37:04 +0200

    Kagiso - Minor changes on _about blade
    - Cleanup code
    - Fixed responsiveness of section
    - Consistency changes

--- resources/views/_about.blade.php
+++ resources/views/_about.blade.php
@@ -1,75 +1,20 @@
-<!--About Us-->
-<!-- <section class="bg-white">
-    <div class="container p-5" id="about-us">
-        <div class="row">
-            <div class="col-sm-6 col-md-3 ">
-                <img src="../images/KayiseIT-Team.jpg" class="rounded shadow my-2">
-                <img src="../images/OurServicesImg.jpg" class="rounded shadow my-2">
-            </div>
-            <div class="col-sm-6 col-md-3 d-flex align-items-center">
-                <img src="../images/KayiseIT-Team.jpg" class="rounded shadow">
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6 d-flex align-items-center">
-                <div class="mt-2 ">
-                    <x-body smheading="About Us" bgheading="We Specialize In Custom Tailored I.T Solutions." paragraph="Welcome to KAYISE IT, a leading IT company specializing in software and web development, as well as providing 4IR skills training. With a decade of experience in the industry, we are passionate about building ICT capacity in communities and young people." btnlink="about" btntext="Discover More" />
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
-
-
 <section class="bg-white">
-<div class="container mx-auto p-5" id="about-us">
-
-    <div class="grid grid-rows-4 grid-flow-col gap-4 flex content-center items-center">
-
-        <div class="col-span-1 row-span-2 h-80 w-64 bg-green-500 rounded-lg relative bg-cover bg-center" style="background-image: url('../images/thando.jpg')">
+    <div class="container md:flex justify-center mx-auto p-5">
+        <!-- images -->
+        <div class="md:flex justify-center items-center md:w-1/2">
+            <!-- images 2 in 1 -->
+            <div>
+                <div class="md:h-48 lg:h-72 md:w-40 lg:w-64 bg-green-500 rounded-lg relative bg-cover bg-center hidden sm:block" style="background-image: url('../images/thando.jpg')"></div>
+                <div class="md:h-48 lg:h-72 md:w-40 lg:w-64 bg-green-500 rounded-lg relative bg-cover bg-center mt-2 hidden sm:block" " style="background-image: url('../images/thando.jpg')"></div>
+            </div>
+            <!-- single image -->
+            <div class="md:h-48 lg:h-72 md:w-40 lg:w-64 bg-green-500 rounded-lg relative bg-cover bg-center md:m-2 my-2 hidden sm:block" " style="background-image: url('../images/thando.jpg')"></div>
         </div>
-        <div class="col-span-1 row-span-2 h-80 w-64 bg-green-500 rounded-lg relative bg-cover bg-center" style="background-image: url('../images/thando.jpg')">
-        </div>
-
-        <div class="row-span-4 col-span-1 h-96 w-64 bg-green-500 rounded-lg relative bg-cover bg-center" style="background-image: url('../images/thando.jpg')">
-        </div>
-
-
-        <div class="row-span-4 col-span-2 p-4">
-            <x-body smheading="About Us" bgheading="We Specialize In Custom Tailored I.T Solutions." paragraph="Welcome to KAYISE IT, a leading IT company specializing in software and web development, as well as providing 4IR skills training. With a decade of experience in the industry, we are passionate about building ICT capacity in communities and young people." btnlink="about" btntext="Discover More" />
+        <!-- content -->
+        <div class="flex items-center md:w-1/2">
+            <x-body smheading="About Us" bgheading="We Specialize In Custom Tailored I.T Solutions."
+                paragraph="Welcome to KAYISE IT, a leading IT company specializing in software and web development, as well as providing 4IR skills training. With a decade of experience in the industry, we are passionate about building ICT capacity in communities and young people."
+                btnlink="about" btntext="Discover More" />
         </div>
     </div>
-
-</div>
 </section>
-
-
-<!-- <div class="container mx-auto p-5" id="about-us">
-    <div class="flex w-11/12">
-        <div class="md:w-1/2 sm:w-full">
-
-            <div class="grid grid-rows-2 grid-flow-col gap-4 flex content-center items-center">
-
-                <div class="col-span-1">
-                    <div class="card border-none rounded-md h-72 w-64 relative bg-cover bg-center" style="background-image: url('../images/thandi.jpg')">
-                    </div>
-                </div>
-
-                <div class="row-span-1 col-span-1">
-                    <div class="card border-none rounded-md h-72 w-64 relative bg-cover bg-center" style="background-image: url('../images/thandi.jpg')">
-                    </div>
-                </div>
-
-                <div class="row-span-2">
-                    <div class="card border-none rounded-md h-72 w-64 relative bg-cover bg-center" style="background-image: url('../images/thandi.jpg')">
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="md:w-1/2 sm:w-full flex justify-center items-center">
-            <div class="">
-                <x-body smheading="About Us" bgheading="We Specialize In Custom Tailored I.T Solutions." paragraph="Welcome to KAYISE IT, a leading IT company specializing in software and web development, as well as providing 4IR skills training. With a decade of experience in the industry, we are passionate about building ICT capacity in communities and young people." btnlink="about" btntext="Discover More" />
-            </div>
-        </div>
-
-    </div>
-</div> -->
