553df723cb3eeecfecd7d7800f9c8b5e7c4f8313
KrayATM <kgothatsotloubatla96@gmail.com>
2023-05-12 15:54:54 +0200

    Removed title animation
    - Removed data-aos class from hero component

--- resources/views/components/hero-banner.blade.php
+++ resources/views/components/hero-banner.blade.php
@@ -1,4 +1,4 @@
 <div class="banner" id="{{$hero}}">
-    <h1 class="primary-title text-center" data-aos="fade-up" data-aos-delay="700">{{$title}}</h1>
+    <h1 class="primary-title text-center">{{$title}}</h1>
 </div>
 <x-breadcrumb></x-breadcrumb>
