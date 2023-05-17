1e80a8a63d5e6cd98ce8e86d645b8eda66e6f5a4
KrayATM <kgothatsotloubatla96@gmail.com>
2023-05-17 10:38:14 +0200

    Kagiso - Minor changes
    Moved the position of the dots on the testimonial carousel

--- resources/views/_testimonials.blade.php
+++ resources/views/_testimonials.blade.php
@@ -34,7 +34,7 @@
                 </div>
             @endforeach
         <!-- Slider indicators -->
-        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-0 left-1/2">
             <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
                 data-carousel-slide-to="0"></button>
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
