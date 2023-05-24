e32dffa69554a92b69bc38ea41576430e6638098
Maluka Thapelo <excellentt7@gmail.com>
2023-05-24 15:40:37 +0200

    viewblogs
    updated the viewblogs blade

--- resources/views/viewblog.blade.php
+++ resources/views/viewblog.blade.php
@@ -1,20 +1,17 @@
-<div class="blog  pt-20">
+<x-app-layout>
+<div class="viewblog  pt-20">
       <div class="row justify-content-center">
             <div class="col-md-10 mb-20">
 
-                  <div class="text-center">
-                        <img src="{{ asset('images/'.$blog->icon)}}" height="300" class="card-img" alt="Your Image">
+                  <div class="text-center flex justify-center">
+                        <img src="{{ asset('images/'.$blog->icon)}}" height="300" class="card-img w-64 h-80 border-none" alt="Your Image">
                   </div>
-                  <h2 style="color: #64bc5c" class="card-title font-bold text-gray-90">{{$blog['title']}}</h2>
+                  
+                  <h2 style="color: #64bc5c" class="card-title font-bold text-gray-90 pt-3 flex justify-center">{{$blog['title']}}</h2>
                   <p class="card-text">{{$blog['content']}}</p>
 
             </div>
       </div>
 </div>
 
-<style>
-      .blog {
-            width: 50%;
-            margin: 50px;
-      }
-</style>
+</x-app-layout>
