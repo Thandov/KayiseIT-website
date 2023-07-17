352540d4e57bedc68c920ef65f8bdb4af9ceef75
MagneciaD <Magneciadladla@gmail.com>
2023-07-17 12:14:00 +0200

    paypal js moved

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -182,5 +182,54 @@
       function showAddNewVoucherForm() {
          document.getElementById("addNewVoucherForm").style.display = "block";
       }
+      //paypal
+      var csrfToken = $('meta[name="csrf-token"]').attr('content');
+
+jQuery(document).ready(function () {
+
+    var totalUSD = $('#paypal-button-container').data('total-usd');
+    paypal.Buttons({
+        createOrder: function (data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: totalUSD.toFixed(2) // Replace with the actual payment amount
+                    }
+                }]
+            });
+        },
+        onApprove: function (data, actions) {
+            return actions.order.capture().then(function (details) {
+                // Perform necessary actions after payment completion
+                // For example, show a success message, save the transaction details, etc.
+
+                if (details['status'] == "COMPLETED") {
+                    save_to_db(details);
+                    // Show the success message
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success',
+                        text: 'Payment was successful!',
+
+                    })
+
+
+                } else {
+                    //this is where we put the code to notify the user that the code was not completed
+                }
+
+            });
+        }
+    }).render('#paypal-button-container');
+
+});
+
+function save_to_db(data) {
+    var details = JSON.stringify(data);
+    document.getElementById("paypal_response").value = details;
+    $('#paypal_form').submit();
+
+}
+
    </script>
 </x-app-layout>
