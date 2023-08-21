d418f28c6dd39cf092636b581036395b807b1c04
KrayATM <129862197+KrayATM@users.noreply.github.com>
2023-08-21 12:59:47 +0200

    Career Mapping Front end Tabs
    - updated composer and npm
    - converted cards to tabs

--- resources/views/career-mapping.blade.php
+++ resources/views/career-mapping.blade.php
@@ -5,6 +5,8 @@
             <!--Page Content-->
             <p class="smalltxt font-bold text-center"><strong>Career Mapping</strong></p>
             <h2 class="bigtxt font-bold text-5xl mb-6 text-center">Occupations</h2>
+            <p class="text-center">Choose your Occupation</p>
+
             <!--Grid-->
             <div class="flex justify-center pt-6">
             <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 w-10/12 justify-center">
@@ -12,7 +14,7 @@
                 @foreach($occupations as $occupation)
                 <a href="{{ url('viewoccupations/'.$occupation->occup_id) }}">
                     <div class="bg-white rounded-lg shadow-md flex items-center justify-center h-64">
-                        <div>
+                        <div class="mx-4">
                             <div  ><img class="h-28 rounded-md mx-auto" src="{{ asset('images/occupations_logo/'.$occupation->image) }}"></div>
                             <h2 class="mt-4 text-xl font-bold smalltxt text-center">{{ $occupation->occupation_name }}</h2>
                         </div>
--- resources/views/viewoccupations.blade.php
+++ resources/views/viewoccupations.blade.php
@@ -1,21 +1,45 @@
 <x-app-layout>
     <section id="career-mapping">
         <div class="container py-5 px-4 md:px-8 max-w-screen-xl mx-auto">
-            <!-- Second page -->
-            <!-- <p class="smalltxt font-bold text-center"><strong>Occupation</strong></p> -->
-            <div class="flex justify-center my-4 rounded-md mx-auto"><img class="h-40" src="{{ asset('images/occupations_logo/'.$occupations->image) }}"></div>
+            <div class="flex justify-center my-4 rounded-md mx-auto">
+                <img class="h-40" src="{{ asset('images/occupations_logo/'.$occupations->image) }}">
+            </div>
             <h2 class="bigtxt font-bold text-5xl mb-3 text-center">{{ $occupations->occupation_name }}</h2>
-            <div class="flex justify-center gap-4">
-                <div class="grid grid-cols-2 gap-4 w-9/12">
-                    @foreach($specializations as $specialization)
-                    
+            <p class="text-center">Choose your specialization</p>
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs justify-center" id="myTabs" role="tablist">
+                @foreach($specializations as $index => $specialization)
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link {{ $index === 0 ? 'active' : '' }}" id="tab{{ $index }}" data-toggle="tab" href="#content{{ $index }}" role="tab" aria-controls="content{{ $index }}" aria-selected="{{ $index === 0 ? 'true' : 'false' }}">
+                            {{ $specialization->specialization_name }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+            
+            <!-- Tab content -->
+            <div class="tab-content" id="myTabContent">
+                @foreach($specializations as $index => $specialization)
+                    <div class="tab-pane fade {{ $index === 0 ? 'show active' : '' }}" id="content{{ $index }}" role="tabpanel" aria-labelledby="tab{{ $index }}">
+                        <!-- Content for each tab -->
                         <a href="{{ route('viewspecialization', $specialization->spec_id) }}">
                             <div class="bg-white rounded-lg shadow-md flex items-center justify-center w-auto h-28">
-                                <h2 class="text-xl font-bold smalltxt text-center">{{$specialization->specialization_name}}</h2>
+                                <h2 class="text-xl font-bold smalltxt text-center">{{ $specialization->specialization_name }}</h2>
                             </div>
                         </a>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
             </div>
+        </div>
     </section>
 </x-app-layout>
+
+<!-- Include necessary Bootstrap and jQuery libraries -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+    // Initialize Bootstrap tabs
+    $(document).ready(function() {
+        $('.nav-tabs a').tab();
+    });
+</script>
