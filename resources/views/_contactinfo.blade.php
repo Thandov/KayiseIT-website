f48628145a10cdb071edf00518120e8ec73987f1
thapeloexcellent <excellentt7@gmail.com>
2024-01-17 09:50:27 +0200

    Update _contactinfo.blade.php

--- resources/views/_contactinfo.blade.php
+++ resources/views/_contactinfo.blade.php
@@ -2,18 +2,17 @@
 <div class="container py-5 px-4 md:px-8 max-w-screen-xl mx-auto">
     <div class="grid grid-cols-1 md:grid-cols-3 justify-center gap-4">
 
-        <div class=" border-none rounded-md w-64 bg-slate-100">
+        <a href="mailto:info@kayiseit.co.za" class=" border-none rounded-md w-64 bg-slate-100">
             <div class="flex justify-center p-2">
                 <div class="h-32 w-32 rounded-full bg-green-500 flex items-center justify-center"><img class="w-12" src="../images/icons/em.png"></div>
             </div>
             <div class="">
                 <h2 class="mb-2 text-lg font-bold smalltxt flex justify-center">Email Address</h2>
                 <p class="text-sm flex justify-center">info@kayiseit.co.za</p>
-                <p class="text-sm flex justify-center">admin@kayiseit.co.za</p>
             </div>
-        </div>
+        </a>
 
-        <div class=" border-none rounded-md w-64 bg-slate-100">
+        <a href="tel:+27877022625" class=" border-none rounded-md w-64 bg-slate-100">
             <div class="flex justify-center p-2">
                 <div class="h-32 w-32 rounded-full bg-green-500 flex items-center justify-center"><img class="w-12" src="../images/icons/ph.png"></div>
             </div>
@@ -21,9 +20,9 @@
                 <h2 class="mb-2 text-lg font-bold smalltxt flex justify-center">Phone</h2>
                 <p class="text-sm flex justify-center">+27 87 702 2625</p>
             </div>
-        </div>
+        </a>
 
-        <div class=" border-none rounded-md w-64 bg-slate-100">
+        <a href="https://maps.app.goo.gl/PANTYMHfVkWBoKSp7" class="border-none rounded-md w-64 bg-slate-100">
             <div class="flex justify-center p-2">
                 <div class="h-32 w-32 rounded-full bg-green-500 flex items-center justify-center"><img class="w-12" src="../images/icons/ad.png"></div>
             </div>
@@ -32,7 +31,7 @@
                 <p class="text-sm flex justify-center">39b Brown str,Nelbro Building,</p>
                 <p class="text-sm flex justify-center">office 2, Nelspruit, 1201</p>
             </div>
-        </div>
+        </a>
 
     </div>
 </div>
